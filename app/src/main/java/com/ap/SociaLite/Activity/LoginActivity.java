76731ec52980d693165680e2523a74f1f619b761
ChiragParmarDeveloper <chirag.parmar1994@gmail.com>
2020-12-23 19:25:41 +0530

    commit new

--- app/src/main/java/com/ap/SociaLite/Activity/LoginActivity.java
+++ app/src/main/java/com/ap/SociaLite/Activity/LoginActivity.java
@@ -39,7 +39,6 @@ public class LoginActivity extends AppCompatActivity {
     @BindView(R.id.password)
     EditText password;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
