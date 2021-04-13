b679dfc4818335a3dcba8c6d9524b6a448c174d9
ChiragParmarDeveloper <chirag.parmar1994@gmail.com>
2021-04-13 13:56:11 +0530

    commit new

--- app/src/main/java/com/ap/SociaLite/Fragment/NetworkFragment.java
+++ app/src/main/java/com/ap/SociaLite/Fragment/NetworkFragment.java
@@ -7,13 +7,13 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ProgressBar;
 
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.ap.SociaLite.Activity.SpotLightActivity;
 import com.ap.SociaLite.Application.Session;
 import com.ap.SociaLite.Editors.EditImageActivity;
-import com.ap.SociaLite.Presenter.CategoryFragmentPresenter;
 import com.ap.SociaLite.Presenter.NetworkFragmentPresenter;
 import com.ap.SociaLite.R;
 
@@ -24,15 +24,17 @@ import butterknife.OnClick;
 public class NetworkFragment extends Fragment {
 
     @BindView(R.id.recycleview_network_post)
-   public RecyclerView recycleview_network_post;
+    public RecyclerView recycleview_network_post;
 
     @BindView(R.id.progressbar)
     public ProgressBar progressbar;
 
-    //    private MyNetworkAdapter myNetworkAdapter;
-//    private RecyclerView.LayoutManager layoutManager;
-//
-//    ArrayList Name = new ArrayList<>(Arrays.asList("Name", "Name", "Name", "Name", "Name"));
+    @BindView(R.id.network_story_constrain)
+    ConstraintLayout network_story_constrain;
+
+    @BindView(R.id.network_image_constrain)
+    ConstraintLayout network_image_constrain;
+
     public String UserId;
 
     @Override
@@ -41,34 +43,26 @@ public class NetworkFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_network, container, false);
         ButterKnife.bind(this, view);
+
         Session session = new Session(getActivity());
         UserId = session.getUser_id();
 
-//        recycleview_network_post = view.findViewById(R.id.recycleview_network_post);
-//        layoutManager = new GridLayoutManager(getActivity(), 1);
-//        //recyclerview.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, true));
-//        recycleview_network_post.setLayoutManager(layoutManager);
-//        myNetworkAdapter = new MyNetworkAdapter(Name, getActivity());
-//        recycleview_network_post.setAdapter(myNetworkAdapter);
-
         new NetworkFragmentPresenter(getActivity(), this).my_network_post(UserId);
-      return view;
+        return view;
     }
 
-//    @OnClick({R.id.network_story_constrain, R.id.network_image_constrain})
-//    public void OnClick(View view) {
-//        switch (view.getId()) {
-//            case R.id.network_story_constrain:
-//                startActivity(new Intent(getActivity(), SpotLightActivity.class));
-//                break;
-//
-//            case R.id.network_image_constrain:
-//                Intent in = new Intent(getActivity(), EditImageActivity.class);
-//                in.putExtra("network_fragment", "my_network");
-//                startActivity(in);
-//                break;
-//        }
-//    }
+    @OnClick({R.id.network_story_constrain,R.id.network_image_constrain})
+    public void OnClick(View view) {
+        switch (view.getId()) {
+            case R.id.network_story_constrain:
+                startActivity(new Intent(getActivity(), SpotLightActivity.class));
+                break;
 
-
+            case R.id.network_image_constrain:
+                Intent in = new Intent(getActivity(), EditImageActivity.class);
+                in.putExtra("network_fragment", "my_network");
+                startActivity(in);
+                break;
+        }
+    }
 }
