c692dd5e3cf90ed3ea3fd3a72cd400099cbe5707
ChiragParmarDeveloper <chirag.parmar1994@gmail.com>
2021-04-29 17:47:39 +0530

    commit new

--- app/src/main/java/com/ap/SociaLite/Activity/Search.java
+++ app/src/main/java/com/ap/SociaLite/Activity/Search.java
@@ -1,16 +1,24 @@
 package com.ap.SociaLite.Activity;
 
 import android.content.Intent;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.SearchView;
+import androidx.core.graphics.drawable.RoundedBitmapDrawable;
+import androidx.core.graphics.drawable.RoundedBitmapDrawableFactory;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.ap.SociaLite.Adapter.SearchProfileAdapter;
@@ -46,18 +54,22 @@ public class Search extends AppCompatActivity {
     @BindView(R.id.txt_connection)
     public TextView txt_connection;
 
+    @BindView(R.id.img_pic)
+    public ImageView img_pic;
+
+
     public SearchProfileAdapter searchProfileAdapter;
     public List<data> datas;
 
     public String user_id, RequestId;
-           public int position;
-
+    public int position;
+    private Resources mResources;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
         ButterKnife.bind(this);
-
+        mResources = getResources();
         Session session = new Session(getApplicationContext());
         user_id = session.getUser_id();
 
@@ -65,7 +77,7 @@ public class Search extends AppCompatActivity {
         filter();
     }
 
-    @OnClick({R.id.img_back, R.id.search_connect, R.id.search_msg, R.id.search_share,R.id.search_profile_user_name,R.id.search_profile_image})
+    @OnClick({R.id.img_back, R.id.search_connect, R.id.search_msg, R.id.search_share, R.id.search_profile_user_name, R.id.search_profile_image})
     public void OnClick(View view) {
         switch (view.getId()) {
             case R.id.img_back:
@@ -73,10 +85,8 @@ public class Search extends AppCompatActivity {
                 break;
 
             case R.id.search_connect:
-            //    txt_connection.setText("Requesting");
                 new SearchPresenter(this, this).send_request(user_id, RequestId);
                 Log.d("adapter_position", String.valueOf(position));
-             //   new SearchPresenter(this, this).all_user(user_id);
                 break;
 
             case R.id.search_msg:
@@ -93,13 +103,13 @@ public class Search extends AppCompatActivity {
 
             case R.id.search_profile_user_name:
                 Intent con_pro1 = new Intent(view.getContext(), ProfileConnectionActivity.class);
-                con_pro1.putExtra("request_id",RequestId);
+                con_pro1.putExtra("request_id", RequestId);
                 startActivity(con_pro1);
                 break;
 
             case R.id.search_profile_image:
                 Intent con_pro = new Intent(view.getContext(), ProfileConnectionActivity.class);
-                con_pro.putExtra("request_id",RequestId);
+                con_pro.putExtra("request_id", RequestId);
                 startActivity(con_pro);
                 break;
 
@@ -148,11 +158,119 @@ public class Search extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
-//        new SearchPresenter(this, this).all_user(user_id);
-//        filter();
+        new SearchPresenter(this, this).all_user(user_id);
+        filter();
 
     }
 
+    public RoundedBitmapDrawable createRoundedBitmapDrawableWithBorder(Bitmap bitmap){
+        int bitmapWidth = bitmap.getWidth();
+        int bitmapHeight = bitmap.getHeight();
+        int borderWidthHalf = 10; // In pixels
+        //Toast.makeText(mContext,""+bitmapWidth+"|"+bitmapHeight,Toast.LENGTH_SHORT).show();
+
+        // Calculate the bitmap radius
+        int bitmapRadius = Math.min(bitmapWidth,bitmapHeight)/2;
+
+        int bitmapSquareWidth = Math.min(bitmapWidth,bitmapHeight);
+        //Toast.makeText(mContext,""+bitmapMin,Toast.LENGTH_SHORT).show();
+
+        int newBitmapSquareWidth = bitmapSquareWidth+borderWidthHalf;
+        //Toast.makeText(mContext,""+newBitmapMin,Toast.LENGTH_SHORT).show();
+
+        /*
+            Initializing a new empty bitmap.
+            Set the bitmap size from source bitmap
+            Also add the border space to new bitmap
+        */
+        Bitmap roundedBitmap = Bitmap.createBitmap(newBitmapSquareWidth,newBitmapSquareWidth,Bitmap.Config.ARGB_8888);
+
+        /*
+            Canvas
+                The Canvas class holds the "draw" calls. To draw something, you need 4 basic
+                components: A Bitmap to hold the pixels, a Canvas to host the draw calls (writing
+                into the bitmap), a drawing primitive (e.g. Rect, Path, text, Bitmap), and a paint
+                (to describe the colors and styles for the drawing).
+
+            Canvas(Bitmap bitmap)
+                Construct a canvas with the specified bitmap to draw into.
+        */
+        // Initialize a new Canvas to draw empty bitmap
+        Canvas canvas = new Canvas(roundedBitmap);
+
+        /*
+            drawColor(int color)
+                Fill the entire canvas' bitmap (restricted to the current clip) with the specified
+                color, using srcover porterduff mode.
+        */
+        // Draw a solid color to canvas
+        canvas.drawColor(Color.RED);
+
+        // Calculation to draw bitmap at the circular bitmap center position
+        int x = borderWidthHalf + bitmapSquareWidth - bitmapWidth;
+        int y = borderWidthHalf + bitmapSquareWidth - bitmapHeight;
+
+        /*
+            drawBitmap(Bitmap bitmap, float left, float top, Paint paint)
+                Draw the specified bitmap, with its top/left corner at (x,y), using the specified
+                paint, transformed by the current matrix.
+        */
+        /*
+            Now draw the bitmap to canvas.
+            Bitmap will draw its center to circular bitmap center by keeping border spaces
+        */
+        canvas.drawBitmap(bitmap, x, y, null);
+
+        // Initializing a new Paint instance to draw circular border
+        Paint borderPaint = new Paint();
+        borderPaint.setStyle(Paint.Style.STROKE);
+        borderPaint.setStrokeWidth(borderWidthHalf*2);
+        borderPaint.setColor(Color.WHITE);
+
+        /*
+            drawCircle(float cx, float cy, float radius, Paint paint)
+                Draw the specified circle using the specified paint.
+        */
+        /*
+            Draw the circular border to bitmap.
+            Draw the circle at the center of canvas.
+         */
+        canvas.drawCircle(canvas.getWidth()/2, canvas.getWidth()/2, newBitmapSquareWidth/2, borderPaint);
+
+        /*
+            RoundedBitmapDrawable
+                A Drawable that wraps a bitmap and can be drawn with rounded corners. You can create
+                a RoundedBitmapDrawable from a file path, an input stream, or from a Bitmap object.
+        */
+        /*
+            public static RoundedBitmapDrawable create (Resources res, Bitmap bitmap)
+                Returns a new drawable by creating it from a bitmap, setting initial target density
+                based on the display metrics of the resources.
+        */
+        /*
+            RoundedBitmapDrawableFactory
+                Constructs RoundedBitmapDrawable objects, either from Bitmaps directly, or from
+                streams and files.
+        */
+        // Create a new RoundedBitmapDrawable
+        RoundedBitmapDrawable roundedBitmapDrawable = RoundedBitmapDrawableFactory.create(mResources,roundedBitmap);
+
+        /*
+            setCornerRadius(float cornerRadius)
+                Sets the corner radius to be applied when drawing the bitmap.
+        */
+        // Set the corner radius of the bitmap drawable
+        roundedBitmapDrawable.setCornerRadius(bitmapRadius);
+
+        /*
+            setAntiAlias(boolean aa)
+                Enables or disables anti-aliasing for this drawable.
+        */
+        roundedBitmapDrawable.setAntiAlias(true);
+
+        // Return the RoundedBitmapDrawable
+        return roundedBitmapDrawable;
+    }
 
 
 }
--- app/src/main/java/com/ap/SociaLite/Adapter/SearchProfileAdapter.java
+++ app/src/main/java/com/ap/SociaLite/Adapter/SearchProfileAdapter.java
@@ -6,11 +6,15 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.core.graphics.drawable.RoundedBitmapDrawable;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.amulyakhare.textdrawable.TextDrawable;
+import com.amulyakhare.textdrawable.util.ColorGenerator;
 import com.ap.SociaLite.Activity.Search;
 import com.ap.SociaLite.Pojo.data;
 import com.ap.SociaLite.R;
@@ -20,6 +24,9 @@ import com.squareup.picasso.Picasso;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class SearchProfileAdapter extends RecyclerView.Adapter<SearchProfileAdapter.MyHolder> {
 
@@ -58,14 +65,33 @@ public class SearchProfileAdapter extends RecyclerView.Adapter<SearchProfileAdap
         holder.name.setText(item.username);
 
         if (search.position == position) {
+          //  holder.profile.setBorderColor(mContext.getResources().getColor(R.color.colorAccent));
             search.RequestId = datas.get(position).request_id;
             search.search_profile_user_name.setText(datas.get(position).username);
-            if (item.profile_pic.equals("http://the-socialite.com/admin/")) {
-                Drawable upload_img = mContext.getDrawable(R.drawable.ic_user_icon);
-                search.search_profile_image.setImageDrawable(upload_img);
+
+         //   search.img_pic.set(mContext.getResources().getColor(R.color.colorAccent));
+            if (datas.get(position).profile_pic.equals("http://the-socialite.com/admin/")) {
+                search.img_pic.setVisibility(View.VISIBLE);
+                String avatarTitle = String.valueOf(datas.get(position).username.charAt(0)).toUpperCase();
+                ColorGenerator generator = ColorGenerator.MATERIAL;
+                int randomcolor = generator.getRandomColor();
+
+                TextDrawable.IBuilder builder = TextDrawable.builder().beginConfig().endConfig().round();
+
+                TextDrawable drawable = builder.build(avatarTitle, randomcolor);
+                search.img_pic.setImageDrawable(drawable);
+
             } else {
+                search.img_pic.setVisibility(View.GONE);
                 Picasso.get().load(item.profile_pic).into(search.search_profile_image);
             }
+
+//            if (item.profile_pic.equals("http://the-socialite.com/admin/")) {
+//                Drawable upload_img = mContext.getDrawable(R.drawable.ic_user_icon);
+//                search.search_profile_image.setImageDrawable(upload_img);
+//            } else {
+//                Picasso.get().load(item.profile_pic).into(search.search_profile_image);
+//            }
 
             for (int i = 0; i < datas.size(); i++) {
                 if (datas.get(position).is_connected.equals("Accepted")) {
@@ -78,25 +104,63 @@ public class SearchProfileAdapter extends RecyclerView.Adapter<SearchProfileAdap
             }
         }
 
+
         if (item.profile_pic.equals("http://the-socialite.com/admin/")) {
-            Drawable upload_img = mContext.getDrawable(R.drawable.ic_user_icon);
-            holder.profile.setImageDrawable(upload_img);
+
+            String avatarTitle = String.valueOf(item.username.charAt(0)).toUpperCase();
+            ColorGenerator generator = ColorGenerator.MATERIAL;
+            int randomcolor = generator.getRandomColor();
+
+            TextDrawable.IBuilder builder = TextDrawable.builder().beginConfig().endConfig().round();
+
+            TextDrawable drawable = builder.build(avatarTitle, randomcolor);
+            holder.viewer_profile.setImageDrawable(drawable);
         } else {
             Picasso.get().load(item.profile_pic).into(holder.profile);
         }
 
+
+
+//        if (item.profile_pic.equals("http://the-socialite.com/admin/")) {
+//            Drawable upload_img = mContext.getDrawable(R.drawable.ic_user_icon);
+//            holder.profile.setImageDrawable(upload_img);
+//        } else {
+//            Picasso.get().load(item.profile_pic).into(holder.profile);
+//        }
+
         holder.itemView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+              //  holder.profile.setBorderColor(mContext.getResources().getColor(R.color.colorAccent));
                 search.RequestId = datas.get(position).request_id;
                 search.position = (position);
-                if (datas.get(position).profile_pic.equals("http://the-socialite.com/admin/")) {
-                    Drawable upload_img = mContext.getDrawable(R.drawable.ic_user_icon);
-                    search.search_profile_image.setImageDrawable(upload_img);
-                } else {
-                    Picasso.get().load(datas.get(position).profile_pic).into(search.search_profile_image);
-                }
+
                 search.search_profile_user_name.setText(datas.get(position).username);
+
+//                if (datas.get(position).profile_pic.equals("http://the-socialite.com/admin/")) {
+//                    search.img_pic.setVisibility(View.VISIBLE);
+//                    Drawable upload_img = mContext.getDrawable(R.drawable.ic_user_icon);
+//                    search.img_pic.setImageDrawable(upload_img);
+//                } else {
+//                    search.img_pic.setVisibility(View.GONE);
+//                    Picasso.get().load(datas.get(position).profile_pic).into(search.search_profile_image);
+//                }
+                if (datas.get(position).profile_pic.equals("http://the-socialite.com/admin/")) {
+                    search.img_pic.setVisibility(View.VISIBLE);
+
+                    String avatarTitle = String.valueOf(datas.get(position).username.charAt(0)).toUpperCase();
+                    ColorGenerator generator = ColorGenerator.MATERIAL;
+                    int randomcolor = generator.getRandomColor();
+
+                    TextDrawable.IBuilder builder = TextDrawable.builder().beginConfig().endConfig().round();
+
+                    TextDrawable drawable = builder.build(avatarTitle, randomcolor);
+                    search.img_pic.setImageDrawable(drawable);
+                } else {
+                    search.img_pic.setVisibility(View.GONE);
+                    Picasso.get().load(datas.get(position).profile_pic).into(search.search_profile_image);
+                  }
+
 
                 for (int i = 0; i < datas.size(); i++) {
                     if (datas.get(position).is_connected.equals("Accepted")) {
@@ -124,9 +188,16 @@ public class SearchProfileAdapter extends RecyclerView.Adapter<SearchProfileAdap
         CircularImageView profile;
         TextView name;
 
+        @BindView(R.id.viewer_profile)
+        ImageView viewer_profile;
+
+        @BindView(R.id.text_user_avatar_title)
+        TextView text_user_avatar_title;
+
+
         public MyHolder(@NonNull View itemView) {
             super(itemView);
-
+            ButterKnife.bind(this, itemView);
             profile = itemView.findViewById(R.id.user_profile);
             name = itemView.findViewById(R.id.user_name);
 
